9a2b696ba5c2399fbca145487b842bd67685a292
Magomed Umkhanov <155390249+umkhanov@users.noreply.github.com>
2026-06-12 23:57:06 +0300

    Add files via upload

---
+++ frontend/mobile/app/_layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }} />
+      <StatusBar style="light" />
+    </>
+  )
+}
